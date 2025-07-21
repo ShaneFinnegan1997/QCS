a7768c2558ff754dfbc69baa128f7d55dd49a5f1
ShaneFinnegan1997 <shanefinnegan1997@gmail.com>
2025-07-20 21:47:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,22 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js"; // Import Firebase Auth
 
 document.addEventListener('DOMContentLoaded', function() {
+
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyD-giQ4CGXX6F0RIXbAzbp_0vDDomoLo8g",
+  authDomain: "qcsweeps-4b994.firebaseapp.com",
+  databaseURL: "https://qcsweeps-4b994-default-rtdb.firebaseio.com",
+  projectId: "qcsweeps-4b994",
+  storageBucket: "qcsweeps-4b994.appspot.com",
+  messagingSenderId: "810241609281",
+  appId: "1:810241609281:web:63ecd22b6acbee2cf480c0"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const auth = getAuth(app); // Initialize Firebase Auth
+
 // Load header/footer
 (async () => {
   const loadHTML = async (selector, url) => {
@@ -23,6 +39,26 @@ document.addEventListener('DOMContentLoaded', function() {
       const res = await fetch(url);
       if (res.ok) {
         document.querySelector(selector).innerHTML = await res.text();
+
+        // Manually trigger the announcement script after loading header.html
+        if (selector === "#header-container") {
+            const announcementTextElement = document.getElementById('announcement-text');
+            const announcementTimestampElement = document.getElementById('announcement-timestamp');
+
+            // Announcement code
+            const announcementRef = ref(db, 'announcement');
+
+            onValue(announcementRef, (snapshot) => {
+                const announcement = snapshot.val();
+                if (announcement && announcement.text) {
+                    announcementTextElement.textContent = announcement.text;
+                    announcementTimestampElement.textContent = "Last Updated: " + (new Date(announcement.timestamp)).toLocaleString();
+                } else {
+                    announcementTextElement.textContent = 'No announcement currently.';
+                    announcementTimestampElement.textContent = "";
+                }
+            });
+        }
       }
     } catch (e) {
       console.error(e);
