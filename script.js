97a6679516b1d5e99639111fa2802f93244724cb
ShaneFinnegan1997 <shanefinnegan1997@gmail.com>
2025-07-20 20:43:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,39 +16,6 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js"; // Import Firebase Auth
 
 document.addEventListener('DOMContentLoaded', function() {
-
-// Firebase Config
-const firebaseConfig = {
-  apiKey: "AIzaSyD-giQ4CGXX6F0RIXbAzbp_0vDDomoLo8g",
-  authDomain: "qcsweeps-4b994.firebaseapp.com",
-  databaseURL: "https://qcsweeps-4b994-default-rtdb.firebaseio.com",
-  projectId: "qcsweeps-4b994",
-  storageBucket: "qcsweeps-4b994.appspot.com",
-  messagingSenderId: "810241609281",
-  appId: "1:810241609281:web:63ecd22b6acbee2cf480c0"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-const auth = getAuth(app); // Initialize Firebase Auth
-
-// Announcement Code
-const announcementRef = ref(db, 'announcement');
-const announcementTextElement = document.getElementById('announcement-text');
-const announcementTimestampElement = document.getElementById('announcement-timestamp');  // Get the timestamp element
-
-onValue(announcementRef, (snapshot) => {
-    const announcement = snapshot.val();
-    if (announcement && announcement.text) { // Changed message to text
-        announcementTextElement.textContent = announcement.text; // Changed message to text
-        announcementTimestampElement.textContent = "Last Updated: " + (new Date(announcement.timestamp)).toLocaleString();
-    } else {
-        announcementTextElement.textContent = 'No announcement currently.';
-        announcementTimestampElement.textContent = "";
-    }
-});
-
-
 // Load header/footer
 (async () => {
   const loadHTML = async (selector, url) => {
